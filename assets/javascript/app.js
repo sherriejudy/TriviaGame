2f73fd5e709af2729ba9852abb60d24ce894bebc
Judy Hu <judy.hu9@gmail.com>
2018-12-31 15:24:19 -0500

    fully functional and responsive

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,149 +1,61 @@
+$("#start").on("click", start);
+$(".answer").on("click", nextQuestion);
+
 var questions = [
   {
-    q: "What is Judy's favourite colour?",
-    a: ["Blue", "Yellow", "Red", "Green"],
-    solution: "Green"
-    // link to gif
+    question: "What is my birthday?",
+    answers: ["Jan", "Feb", "Mar", "Dec"],
+    solution: "Jan"
   },
   {
-    q: "What is Judy's favourite dog breed?",
-    a: ["Pug", "Rottweiller", "Chow Chow", "Golden Retreiver"],
-    solution: "Pug"
-    // link to gif
+    question: "What is Judy's birthmonth?",
+    answers: ["Jan", "Feb", "Aug", "Dec"],
+    solution: "Aug"
   },
   {
-    q: "What is Judy's favourite cat breed?",
-    a: ["Scottish Fold", "Egyption", "Tabby", "Garfield"],
-    solution: "Scottish Fold"
-    // link to gif
-  },
-  {
-    q: "What is the capital Bulgaria?",
-    a: ["Prague", "Sofia", "Helsinki", "Riga"],
-    solution: "Sofia"
-    // link to gif
-  },
-  {
-    q: "What is the capital of Kenya?",
-    a: ["Nairobi", "Mogadishu", "Lagos", "Cheyna"],
-    solution: "Nairobi"
-    // link to gif
+    question: "What is blue?",
+    answers: ["color", "car", "box", "no"],
+    solution: "color"
   }
 ];
-const TIME_LIMIT = 10;
-
+var time = 3;
+var stopwatch;
 var correct = 0;
 var incorrect = 0;
 var unanswered = questions.length;
-var time = TIME_LIMIT;
-var stopWatch;
-var questionNumber = 0;
-var currentSolution;
-var answerTimer;
-// Click Listeners
-$("#start").on("click", start);
-$(".answer").on("click", function() {
-  answerWith(this);
-});
 
-// Start function
 function start() {
-  $("#timer").text("Time Remaining: " + time);
   $("#start").hide();
-  updateText();
-  setUpQuestion();
-}
-
-function setUpQuestion() {
-  stopWatch = setInterval(timer, 1000);
-  var currentQuestion = questions[questionNumber];
-  currentSolution = currentQuestion.solution;
-
-  $("#question").text(currentQuestion.q);
-  $("#answers").removeClass("hidden");
-  $(".answer").each(function(i) {
-    $(this).text(currentQuestion.a[i]);
-  });
-}
-
-function answerWith(answer) {
   resetTimer();
-  unanswered--;
-  if (currentSolution === $(answer).text()) {
-    //Correct Solution
-    correct++;
-    showCorrectScreen();
-  } else {
-    //Incorrect Solution
-    incorrect++;
-    showIncorrectScreen();
-  }
-  answerTimer = setInterval(nextQuestion, 2000);
-  updateText();
+  updateStats();
+  //Set up questions
+  setupQuestions();
 }
-
-function updateText() {
+function resetTimer() {
+  $("#timer").text("Time Left: " + time);
+  stopwatch = setInterval(reduceTime, 1000);
+}
+function reduceTime() {
+  time--;
+  if (time > 0) {
+    $("#timer").text("Time Left: " + time);
+  } else {
+    $("#timer").text("Out of Time!!!");
+    clearInterval(stopwatch);
+  }
+}
+function updateStats() {
   $("#correct").text("Correct: " + correct);
   $("#incorrect").text("Incorrect: " + incorrect);
   $("#unanswered").text("Unanswered: " + unanswered);
 }
-
-function showCorrectScreen() {
-  showCorrectAnswer();
-}
-function showIncorrectScreen() {
-  showCorrectAnswer();
+function setupQuestions() {
+  $("#answers").removeClass("hidden");
+  $("#question").text(questions[0].question);
+  for (var i = 0; i < 4; i++) {
+    $("#" + i).text(questions[0].answers[i]);
+  }
 }
 function nextQuestion() {
-  questionNumber++;
-  if (questionNumber == questions.length) {
-    showEndScreen();
-  } else {
-    clearInterval(answerTimer);
-    resetButtons();
-    setUpQuestion();
-  }
-}
-function showEndScreen() {
-  $("#question").text("COMPLETE");
-  $("#answers").addClass("hidden");
-  $("#timer").addClass("hidden");
-}
-function timer() {
-  if (time > 0) {
-    time--;
-    $("#timer").text("Time Remaining: " + time);
-  } else {
-    outOfTime();
-  }
-}
-function resetTimer() {
-  clearInterval(stopWatch);
-  time = TIME_LIMIT;
-  $("#timer").text("Time Remaining: " + time);
-}
-function outOfTime() {
-  resetTimer();
-  $("#timer").text("Out Of Time");
-  showCorrectAnswer();
-}
-
-function showCorrectAnswer() {
-  for (var i = 1; i < 5; i++) {
-    if ($("#" + i).text() == questions[questionNumber].solution) {
-      // Correct answer
-      $("#" + i).addClass("btn-success");
-    } else {
-      // Incorrect answers
-      $("#" + i).addClass("btn-danger");
-    }
-    $("#" + i).prop("disabled", true);
-  }
-}
-function resetButtons() {
-  for (var i = 1; i < 5; i++) {
-    $("#" + i).removeClass("btn-success");
-    $("#" + i).removeClass("btn-danger");
-    $("#" + i).prop("disabled", false);
-  }
+  console.log("CLicked");
 }
